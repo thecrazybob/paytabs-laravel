06b8bc93deda99d3d6cbaca46e74241ed8cb0a40
thecrazybob <thecrazybob@users.noreply.github.com>
2020-09-04 14:02:52 +0000

    Fix styling

--- src/PaytabsServiceProvider.php
+++ src/PaytabsServiceProvider.php
@@ -19,7 +19,7 @@ class PaytabsServiceProvider extends ServiceProvider
             ], 'views');
 
             $migrationFileName = 'create_paytabs_laravel_table.php';
-            if (!$this->migrationFileExists($migrationFileName)) {
+            if (! $this->migrationFileExists($migrationFileName)) {
                 $this->publishes([
                     __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
                 ], 'migrations');
